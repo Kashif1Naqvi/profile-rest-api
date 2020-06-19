7445aa97c4fa84af18d968821821eb812ce7c5fa
Kashif1Naqvi <mrkashif029@gmail.com>
2020-06-19 11:10:43 +0500

    Add hello world api view

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+class HelloWord(APIView):
+    def get(self,request,format=None):
+        get_api = [
+        'we can get put get patch delete post request',
+        'api view is great for doing programming',
+        'section coding is awersome',
+        'long live pakistan!'
+        ]
+        return Response({
+            'hello':"Hello world",
+            'get_api':get_api
+        })
