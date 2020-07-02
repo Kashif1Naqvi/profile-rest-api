b3784a621115e2bdd7b967515d6e516396c23e28
Kashif1Naqvi <mrkashif029@gmail.com>
2020-07-02 12:23:37 +0500

    code updated

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,27 @@
-import React from 'react'
-
-
+import React,{useState,useEffect} from 'react'
+import {api} from '../api'
+import User from './users/User'
 const Home = () => {
+  const [users,setUser] = useState([])
+  useEffect(()=>{
+    const fetchData = async () => {
+      let response = await fetch(`${api()}/profile_api/profile/`,{
+        method:'GET',
+        headers:{
+          'Content-Type':'application/json',
+        },
+        mode:'cors'
+      })
+      let data = await response.json()
+      setUser(data)
+    }
+    fetchData()
+  },[])
+  const userList = users.map((user)=><User {...user} key={user.id} />)
   return(
-    <div></div>
+    <div>
+      {userList}
+    </div>
   )
 }
 
---
+++ client/src/components/users/User.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const User = (props) => {
+  console.log(props.name);
+  return(
+    <div className="card">
+        <div className="card-body">
+        </div>
+    </div>
+  )
+}
+
+export default User
